5a19322fda290edff529668dd6abe72135507cba
dragossmihai <32205066+dragossmihai@users.noreply.github.com>
2017-10-29 17:42:32 -0500

    zz

--- weather.js
+++ weather.js
@@ -26,17 +26,42 @@
 
 // .append() appends a string (containing HTML) to a jQuery DOM object
 
+
 let handleWeatherResponse = function(response) {
   // leave these two lines alone; they allow for the inspection of
   // the response object in the browser console
   console.log(response)
   window.response = response
 
-  // **** your code starts here - don't modify anything else. you will be sad.
+// **** your code starts here - don't modify anything else. you will be sad.
 
+// DOM for current weather
+
+$("#current-conditions-icon").empty();
+$("#current-conditions-text").empty();
+$(".forecast").empty();
+$("#current-conditions-icon").append(Math.round(response.currently.temperature) + " °F");
+$("#location").append(window.response);
+$("#current-conditions-text").append(response.daily.summary);
+$(".row current").fadeIn(2000); // doesnt work for now
+
+// LOOP for forecast
+
+let markup = "";
+for(let i=0; i<6; i++) {
+   markup += "<div class='col'>";
+     markup += "<h3>" + icon(response.daily.data[i]) + "</i></h3>";
+     markup += "<h4>" + Math.round(response.daily.data[i].temperatureHigh) + " °F" + " | " + Math.round(response.daily.data[i].temperatureLow) + " °F" +"</h4>";
+     markup += "<h5>" + response.daily.data[i].summary + "</h5>";
+   markup += "</div>"
+  }
+
+$(".forecast").append(markup);
+$(".forecast").fadeIn(2000); // doesnt work for now
 
   // *** your code ends here - no, really.
 };
+
 
 // leave this alone; does the event handling and ajax
 $(function() {
